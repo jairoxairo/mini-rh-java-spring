a605bfb783db9401769df3b5add06e84b6caf2d3
Jairo Souza <xairo_jairo@hotmail.com>
2025-09-13 23:14:37 -0300

    feat: configuração iniciar do projeto spring boot

---
+++ src/main/java/com/jairosouza/MiniRH/MiniRhApplication.java
@@ -0,0 +1,13 @@
+package com.jairosouza.MiniRH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniRhApplication.class, args);
+	}
+
+}
